409b25d3e007cdc6636aa081989303f7de64434f
alexpodelinskii <nikitik00787@gmail.com>
2023-11-14 18:50:45 +0300

    feat: add solution to 5, 6 tasks

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -107,8 +107,12 @@ function getLinearEquationRoot(a, b) {
  *   (0,-1) (1,0)    => π/2
  *   (0,1) (0,1)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const scal = x1 * x2 + y1 * y2;
+  const a = (x1 ** 2 + y1 ** 2) ** 0.5;
+  const b = (x2 ** 2 + y2 ** 2) ** 0.5;
+  const rad = scal / (a * b);
+  return Math.acos(rad);
 }
 
 /**
@@ -124,8 +128,8 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  return value % 10;
 }
 
 /**
